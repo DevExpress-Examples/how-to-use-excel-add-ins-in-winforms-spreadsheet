e779c206102a31f8947d5ab4d9bb20f8fc667c2e
DevExpressExampleBot <fakeEmail>
2018-04-24 22:11:15 +0300

    Source auto update [skip ci]

--- VB/SpreadsheetAddIn/Form1.vb
+++ VB/SpreadsheetAddIn/Form1.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -14,110 +13,117 @@ Imports System.Globalization
 Imports DevExpress.Spreadsheet
 
 Namespace SpreadsheetAddIn
-	Partial Public Class Form1
-		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
-		Private workbook As IWorkbook
-		Private excelHelper As ExcelAppHelper
-		Private path As String = System.Windows.Forms.Application.StartupPath
+    Partial Public Class Form1
+        Inherits DevExpress.XtraBars.Ribbon.RibbonForm
 
-		Public Sub New()
-			InitializeComponent()
-			workbook = spreadsheetControl1.Document
-			workbook.LoadDocument("Documents\SphereMaterial.xlsx", DocumentFormat.OpenXml)
-			DefineCustomFunctions()
-			CalculateCustomFunction()
-		End Sub
+        Private workbook As IWorkbook
+        Private excelHelper As ExcelAppHelper
+        Private path As String = System.Windows.Forms.Application.StartupPath
 
-		Private Sub DefineCustomFunctions()
-			' Open Excel Add-In file in the background.
-			excelHelper = New ExcelAppHelper()
+        Public Sub New()
+            InitializeComponent()
+            workbook = spreadsheetControl1.Document
+            workbook.LoadDocument("Documents\SphereMaterial.xlsx", DocumentFormat.OpenXml)
+            DefineCustomFunctions()
+            CalculateCustomFunction()
+        End Sub
 
-			If (Not excelHelper.Initialize(path & "\AddIns\SphereMassAddIn.xlam")) Then
-				MessageBox.Show("Can not start Excel application")
-				Return
-			End If
+        Private Sub DefineCustomFunctions()
+            ' Open Excel Add-In file in the background.
+            excelHelper = New ExcelAppHelper()
 
-			' Specify a new instance of the custom function and add it to the collection of custom functions in a workbook.
-			Dim [function] As New AddInFunction("SPHEREMASS", excelHelper)
-			spreadsheetControl1.Document.CustomFunctions.Add([function])
-		End Sub
+            If Not excelHelper.Initialize(path & "\AddIns\SphereMassAddIn.xlam") Then
+                MessageBox.Show("Can not start Excel application")
+                Return
+            End If
 
-		Private Sub CalculateCustomFunction()
-			Dim worksheet As DevExpress.Spreadsheet.Worksheet = workbook.Worksheets(0)
-			worksheet.Range("E4:E8").ArrayFormula = "=SPHEREMASS(D4:D8, C4:C8)"
-		End Sub
+            ' Specify a new instance of the custom function and add it to the collection of custom functions in a workbook.
+            Dim [function] As New AddInFunction("SPHEREMASS", excelHelper)
+            spreadsheetControl1.Document.CustomFunctions.Add([function])
+        End Sub
 
-		Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles MyBase.FormClosing
-			If excelHelper IsNot Nothing Then
-				excelHelper.Dispose()
-			End If
-		End Sub
+        Private Sub CalculateCustomFunction()
+            Dim worksheet As DevExpress.Spreadsheet.Worksheet = workbook.Worksheets(0)
+            worksheet.Range("E4:E8").ArrayFormula = "=SPHEREMASS(D4:D8, C4:C8)"
+        End Sub
 
-		Public Class AddInFunction
-			Implements ICustomFunction
-			Private Shared parameters_Renamed() As ParameterInfo = PrepareParameters()
-			Private Shared ErrorToValueDictonary As Dictionary(Of Integer, DevExpress.Spreadsheet.CellValue) = CreateErrorToValueDictionary()
-			Private Shared ValueToErrorDictonary As Dictionary(Of DevExpress.Spreadsheet.ErrorValueInfo, Integer) = CreateValueToErrorDictionary()
+        Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+            If excelHelper IsNot Nothing Then
+                excelHelper.Dispose()
+            End If
+        End Sub
 
-			Private Shared Function CreateErrorToValueDictionary() As Dictionary(Of Integer, DevExpress.Spreadsheet.CellValue)
-				Dim result As New Dictionary(Of Integer, DevExpress.Spreadsheet.CellValue)()
-				result.Add(-2146826281, DevExpress.Spreadsheet.CellValue.ErrorDivisionByZero)
-				result.Add(-2146826246, DevExpress.Spreadsheet.CellValue.ErrorValueNotAvailable)
-				result.Add(-2146826259, DevExpress.Spreadsheet.CellValue.ErrorName)
-				result.Add(-2146826288, DevExpress.Spreadsheet.CellValue.ErrorNullIntersection)
-				result.Add(-2146826252, DevExpress.Spreadsheet.CellValue.ErrorNumber)
-				result.Add(-2146826265, DevExpress.Spreadsheet.CellValue.ErrorReference)
-				result.Add(-2146826273, DevExpress.Spreadsheet.CellValue.ErrorInvalidValueInFunction)
-				Return result
-			End Function
+        Public Class AddInFunction
+            Implements ICustomFunction
 
-			Private Shared Function CreateValueToErrorDictionary() As Dictionary(Of DevExpress.Spreadsheet.ErrorValueInfo, Integer)
-				Dim result As New Dictionary(Of DevExpress.Spreadsheet.ErrorValueInfo, Integer)()
-				For Each pair As KeyValuePair(Of Integer, DevExpress.Spreadsheet.CellValue) In ErrorToValueDictonary
-					result.Add(pair.Value.ErrorValue, pair.Key)
-				Next pair
-				Return result
-			End Function
-			Private ReadOnly excelApp As ExcelAppHelper
-			Private ReadOnly name_Renamed As String
 
-			Public Sub New(ByVal name As String, ByVal excelApp As ExcelAppHelper)
-				Me.name_Renamed = name
-				Me.excelApp = excelApp
-			End Sub
+            Private Shared parameters_Renamed() As ParameterInfo = PrepareParameters()
+            Private Shared ErrorToValueDictonary As Dictionary(Of Integer, DevExpress.Spreadsheet.CellValue) = CreateErrorToValueDictionary()
+            Private Shared ValueToErrorDictonary As Dictionary(Of DevExpress.Spreadsheet.ErrorValueInfo, Integer) = CreateValueToErrorDictionary()
 
-			Private Shared Function PrepareParameters() As ParameterInfo()
-				' Missing optional parameters do not result in error message.
-				Return New ParameterInfo() { New ParameterInfo(ParameterType.Value, ParameterAttributes.Required), New ParameterInfo(ParameterType.Value, ParameterAttributes.Optional) }
-			End Function
+            Private Shared Function CreateErrorToValueDictionary() As Dictionary(Of Integer, DevExpress.Spreadsheet.CellValue)
+                Dim result As New Dictionary(Of Integer, DevExpress.Spreadsheet.CellValue)()
+                result.Add(-2146826281, DevExpress.Spreadsheet.CellValue.ErrorDivisionByZero)
+                result.Add(-2146826246, DevExpress.Spreadsheet.CellValue.ErrorValueNotAvailable)
+                result.Add(-2146826259, DevExpress.Spreadsheet.CellValue.ErrorName)
+                result.Add(-2146826288, DevExpress.Spreadsheet.CellValue.ErrorNullIntersection)
+                result.Add(-2146826252, DevExpress.Spreadsheet.CellValue.ErrorNumber)
+                result.Add(-2146826265, DevExpress.Spreadsheet.CellValue.ErrorReference)
+                result.Add(-2146826273, DevExpress.Spreadsheet.CellValue.ErrorInvalidValueInFunction)
+                Return result
+            End Function
 
-			#Region "ICustomFunction Members"
-            Public ReadOnly Property Name() As String Implements ICustomFunction.Name
+            Private Shared Function CreateValueToErrorDictionary() As Dictionary(Of DevExpress.Spreadsheet.ErrorValueInfo, Integer)
+                Dim result As New Dictionary(Of DevExpress.Spreadsheet.ErrorValueInfo, Integer)()
+                For Each pair As KeyValuePair(Of Integer, DevExpress.Spreadsheet.CellValue) In ErrorToValueDictonary
+                    result.Add(pair.Value.ErrorValue, pair.Key)
+                Next pair
+                Return result
+            End Function
+            Private ReadOnly excelApp As ExcelAppHelper
+
+            Private ReadOnly name_Renamed As String
+
+            Public Sub New(ByVal name As String, ByVal excelApp As ExcelAppHelper)
+                Me.name_Renamed = name
+                Me.excelApp = excelApp
+            End Sub
+
+            Private Shared Function PrepareParameters() As ParameterInfo()
+                ' Missing optional parameters do not result in error message.
+                Return New ParameterInfo() { _
+                    New ParameterInfo(ParameterType.Value, ParameterAttributes.Required), _
+                    New ParameterInfo(ParameterType.Value, ParameterAttributes.Optional) _
+                }
+            End Function
+
+            #Region "ICustomFunction Members"
+            Public ReadOnly Property Name() As String
                 Get
                     Return name_Renamed
                 End Get
             End Property
-            Public ReadOnly Property Parameters() As ParameterInfo() Implements ICustomFunction.Parameters
+            Public ReadOnly Property Parameters() As ParameterInfo()
                 Get
                     Return parameters_Renamed
                 End Get
             End Property
-            Public ReadOnly Property ReturnType() As ParameterType Implements ICustomFunction.ReturnType
+            Public ReadOnly Property ReturnType() As ParameterType
                 Get
                     Return ParameterType.Value
                 End Get
             End Property
-			' Do not reevaluate cells on every recalculation.
-            Public ReadOnly Property Volatile() As Boolean Implements ICustomFunction.Volatile
+            ' Do not reevaluate cells on every recalculation.
+            Public ReadOnly Property Volatile() As Boolean
                 Get
                     Return False
                 End Get
             End Property
-            Public Function GetName(ByVal culture As CultureInfo) As String Implements ICustomFunction.GetName
+            Public Function GetName(ByVal culture As CultureInfo) As String
                 Return name_Renamed
             End Function
 
-            Public Function Evaluate(ByVal parameters As IList(Of ParameterValue), ByVal context As EvaluationContext) As ParameterValue Implements ICustomFunction.Evaluate
+            Public Function Evaluate(ByVal parameters As IList(Of ParameterValue), ByVal context As EvaluationContext) As ParameterValue
                 Dim args As New List(Of Object)()
                 ' Provide conversion of function parameters for further evaluation in Excel.
                 For i As Integer = 0 To parameters.Count - 1
@@ -128,97 +134,97 @@ Namespace SpreadsheetAddIn
                 ' Convert the function result value to the SpreadsheetControl's value for the correct displaying.
                 Return ConvertResultValue(objResult)
             End Function
-			#End Region
+            #End Region
 
-			#Region "ParameterValue->object conversion"
-			' Convert the SpreadsheetControl's parameters to Excel parameters. 
-			Private Function ConvertParameter(ByVal parameter As ParameterValue) As Object
-				If parameter.IsNumeric Then
-					Return parameter.NumericValue
-				ElseIf parameter.IsBoolean Then
-					Return parameter.BooleanValue
-				ElseIf parameter.IsText Then
-					Return parameter.TextValue
-				ElseIf parameter.IsError Then
-					Return ValueToErrorDictonary(parameter.ErrorValue)
-				ElseIf parameter.IsRange Then
-					Return ConvertRefParameter(parameter.RangeValue)
-				ElseIf parameter.IsArray Then
-					Return ConvertArrayParameter(parameter.ArrayValue)
-				Else
-					Return System.Reflection.Missing.Value
-				End If
-			End Function
+            #Region "ParameterValue->object conversion"
+            ' Convert the SpreadsheetControl's parameters to Excel parameters. 
+            Private Function ConvertParameter(ByVal parameter As ParameterValue) As Object
+                If parameter.IsNumeric Then
+                    Return parameter.NumericValue
+                ElseIf parameter.IsBoolean Then
+                    Return parameter.BooleanValue
+                ElseIf parameter.IsText Then
+                    Return parameter.TextValue
+                ElseIf parameter.IsError Then
+                    Return ValueToErrorDictonary(parameter.ErrorValue)
+                ElseIf parameter.IsRange Then
+                    Return ConvertRefParameter(parameter.RangeValue)
+                ElseIf parameter.IsArray Then
+                    Return ConvertArrayParameter(parameter.ArrayValue)
+                Else
+                    Return System.Reflection.Missing.Value
+                End If
+            End Function
 
-			Private Function ConvertArrayParameter(ByVal parameter(,) As DevExpress.Spreadsheet.CellValue) As Object(,)
-				Dim height As Integer = parameter.GetLength(0)
-				Dim width As Integer = parameter.GetLength(1)
-				Dim result(,) As Object = CType(Array.CreateInstance(GetType(Object), New Integer() { height, width }, New Integer() { 1, 1 }), Object(,))
-				For i As Integer = 0 To height - 1
-					For j As Integer = 0 To width - 1
-						Dim value As DevExpress.Spreadsheet.CellValue = parameter(i, j)
-						If value.IsEmpty Then
-							result(i + 1, j + 1) = Nothing
-						Else
-							result(i + 1, j + 1) = value
-						End If
-					Next j
-				Next i
-				Return result
-			End Function
+            Private Function ConvertArrayParameter(ByVal parameter(,) As DevExpress.Spreadsheet.CellValue) As Object(,)
+                Dim height As Integer = parameter.GetLength(0)
+                Dim width As Integer = parameter.GetLength(1)
+                Dim result(,) As Object = CType(Array.CreateInstance(GetType(Object), { height, width }, { 1, 1 }), Object(,))
+                For i As Integer = 0 To height - 1
+                    For j As Integer = 0 To width - 1
+                        Dim value As DevExpress.Spreadsheet.CellValue = parameter(i, j)
+                        If value.IsEmpty Then
+                            result(i + 1, j + 1) = Nothing
+                        Else
+                            result(i + 1, j + 1) = value
+                        End If
+                    Next j
+                Next i
+                Return result
+            End Function
 
-			Private Function ConvertRefParameter(ByVal parameter As DevExpress.Spreadsheet.Range) As Object(,)
-				Dim height As Integer = parameter.RowCount
-				Dim width As Integer = parameter.ColumnCount
-				Dim result(,) As Object = CType(Array.CreateInstance(GetType(Object), New Integer() { height, width }, New Integer() { 1, 1 }), Object(,))
-				For i As Integer = 0 To height - 1
-					For j As Integer = 0 To width - 1
-						Dim value As DevExpress.Spreadsheet.CellValue = parameter(i, j).Value
-						result(i + 1, j + 1) = ConvertParameter(value)
-					Next j
-				Next i
-				Return result
-			End Function
-			#End Region
+            Private Function ConvertRefParameter(ByVal parameter As DevExpress.Spreadsheet.Range) As Object(,)
+                Dim height As Integer = parameter.RowCount
+                Dim width As Integer = parameter.ColumnCount
+                Dim result(,) As Object = CType(Array.CreateInstance(GetType(Object), { height, width }, { 1, 1 }), Object(,))
+                For i As Integer = 0 To height - 1
+                    For j As Integer = 0 To width - 1
+                        Dim value As DevExpress.Spreadsheet.CellValue = parameter(i, j).Value
+                        result(i + 1, j + 1) = ConvertParameter(value)
+                    Next j
+                Next i
+                Return result
+            End Function
+            #End Region
 
-			#Region "object->ParameterValue conversion"
-			' Convert Excel parameters to the SpreadsheetControl's parameters.
-			Private Function ConvertResultValue(ByVal value As Object) As ParameterValue
-				Dim result As ParameterValue
-				Dim objArrayRes As Array = TryCast(value, Array)
-				If objArrayRes IsNot Nothing Then
-					Dim height As Integer = objArrayRes.GetLength(0)
-					Dim lowerY As Integer = objArrayRes.GetLowerBound(0)
-					Dim width As Integer = objArrayRes.GetLength(1)
-					Dim lowerX As Integer = objArrayRes.GetLowerBound(1)
-					Dim arrayResult(height - 1, width - 1) As DevExpress.Spreadsheet.CellValue
-					For i As Integer = 0 To height - 1
-						For j As Integer = 0 To width - 1
-							arrayResult(i, j) = ConvertResultValueCore(objArrayRes.GetValue(i + lowerY, j + lowerX))
-						Next j
-					Next i
-					result = arrayResult
-				Else
-					result = ConvertResultValueCore(value)
-				End If
-				Return result
-			End Function
+            #Region "object->ParameterValue conversion"
+            ' Convert Excel parameters to the SpreadsheetControl's parameters.
+            Private Function ConvertResultValue(ByVal value As Object) As ParameterValue
+                Dim result As ParameterValue
+                Dim objArrayRes As Array = TryCast(value, Array)
+                If objArrayRes IsNot Nothing Then
+                    Dim height As Integer = objArrayRes.GetLength(0)
+                    Dim lowerY As Integer = objArrayRes.GetLowerBound(0)
+                    Dim width As Integer = objArrayRes.GetLength(1)
+                    Dim lowerX As Integer = objArrayRes.GetLowerBound(1)
+                    Dim arrayResult(height - 1, width - 1) As DevExpress.Spreadsheet.CellValue
+                    For i As Integer = 0 To height - 1
+                        For j As Integer = 0 To width - 1
+                            arrayResult(i, j) = ConvertResultValueCore(objArrayRes.GetValue(i + lowerY, j + lowerX))
+                        Next j
+                    Next i
+                    result = arrayResult
+                Else
+                    result = ConvertResultValueCore(value)
+                End If
+                Return result
+            End Function
 
             Private Function ConvertResultValueCore(ByVal value As Object) As DevExpress.Spreadsheet.CellValue
                 If value Is Nothing Then
                     Return DevExpress.Spreadsheet.CellValue.Empty
                 End If
                 If TypeOf value Is Integer Then
-                    If ErrorToValueDictonary.ContainsKey(CType(value, Integer)) Then
-                        Return ErrorToValueDictonary(CType(value, Integer))
+                    If ErrorToValueDictonary.ContainsKey(value) Then
+                        Return ErrorToValueDictonary(value)
                     Else
-                        Return DevExpress.Spreadsheet.CellValue.FromObject(value)
+                        Return value
                     End If
                 End If
-                Return DevExpress.Spreadsheet.CellValue.FromObject(value)
+                Return value
             End Function
-			#End Region
+            #End Region
 
-		End Class
-	End Class
+        End Class
+    End Class
 End Namespace
